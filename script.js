d659dc0aa052b869d6962bf1ff4bf6af7fed2a50
shonenjumplab <f.mezzane@gmail.com>
2024-12-15 20:56:30 +0100

    nn

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const body = document.body;
 const openSidebar=document.querySelector('#openSidebar');
 const closeSidebar=document.querySelector('#closeSidebar');
-const toggleTheme=document.querySelector('toggle-theme');
-const sidebar=document.querySelector('main-sidebar');
+const toggleTheme=document.querySelector('.toggle-theme');
+const sidebar=document.querySelector('.main-sidebar');
 const light=toggleTheme.children[0];
 const dark=toggleTheme.children[1];
 const percentage = document.querySelectorAll('percentage p');
