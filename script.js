c96026d804f3e7d3d4953c754698696064fa3b92
shonenjumplab <f.mezzane@gmail.com>
2024-12-15 20:40:46 +0100

    mess

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const body = document.body;
 const openSidebar=document.querySelector('#openSidebar');
 const closeSidebar=document.querySelector('#closeSidebar');
-const toggleTheme=document.querySelector('.toggle-theme');
-const sidebar=document.querySelector('.main-sidebar');
+const toggleTheme=document.querySelector('toggle-theme');
+const sidebar=document.querySelector('main-sidebar');
 const light=toggleTheme.children[0];
 const dark=toggleTheme.children[1];
-const percentage = document.querySelectorAll('.percentage p');
+const percentage = document.querySelectorAll('percentage p');
 
 openSidebar.addEventListener('click',()=>{
     sidebar.style.left='0%';
