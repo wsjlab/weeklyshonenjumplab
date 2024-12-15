3727d46e6293e88173b6d064fc0d4fc2812f991e
shonenjumplab <f.mezzane@gmail.com>
2024-12-15 13:37:25 +0100

    sdebar

--- script.js
+++ script.js
@@ -9,11 +9,33 @@ const percentage = document.querySelectorAll('.percentage p');
 
 openSidebar.addevenlistener('click',()=>{
     sidebar.style.Left='0%'
-})
+});
 closeSidebar.addevenlistener('click',()=>{
     sidebar.style.Left='-100%'  
-})
+});
 
+toggleTheme.addevenlistener('click',changeTheme)
 
+function changeTheme(){
+    if(body.classList.contains('dark-mode')){
+        lightMode();
+    }else if(!body.classList.contains('dark-mode')){
+        darkMode();
+    }
+}
 
+if (window.matchMedia('(prefer-color-scheme:dark)').matches) {
+    darkMode();
+}
 
+function lightMode(){
+    body.classlist.remove('dark-mode');
+    light.classList.add('active');
+    dark.classList.remove('active');
+
+}
+function darkMode(){
+    body.classlist.add('dark-mode');
+    light.classList.remove('active');
+    dark.classList.add('active');
+}
