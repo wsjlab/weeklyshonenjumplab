2f1f3a5882dead400ef2a3f5ae94ee898eb1c34f
shonenjumplab <f.mezzane@gmail.com>
2024-12-20 18:20:57 +0100

    lets

--- script.js
+++ script.js
@@ -42,13 +42,10 @@ function darkMode(){
 
 //charts and buttons
 
-const supabaseUrl = 'https://hzamsiqeeziwhfpilneo.supabase.co'; // Replace with your Supabase URL  
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6YW1zaXFlZXppd2hmcGlsbmVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5NDg0NTUsImV4cCI6MjA0OTUyNDQ1NX0.xQMdpOzBxmpQ1zeP_kljS9IolBVTsJKqbl4g6JuhzGI'; // Replace with your Supabase public anon key  
 
-// Create Supabase client  
-const { createClient } = supabase;
-const supabaseClient = createClient(supabaseUrl, supabaseKey);
-console.log(supabaseClient); // Corrected from `console.alerte`
+const supabaseUrl = 'https://hzamsiqeeziwhfpilneo.supabase.co';
+const supabaseKey =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6YW1zaXFlZXppd2hmcGlsbmVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5NDg0NTUsImV4cCI6MjA0OTUyNDQ1NX0.xQMdpOzBxmpQ1zeP_kljS9IolBVTsJKqbl4g6JuhzGI'; // Replace with your Supabase public anon key  
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 async function fetchClassement() {
     const { data, error } = await supabase
@@ -62,8 +59,8 @@ async function fetchClassement() {
         const classements = data.map(item => item.classement);
         const auteurs = data.map(item => item.auteur);
 
-        const supabaseClient = document.getElementById('classementChart').getContext('2d');
-        new Chart(supabaseClient, {
+        const ctx = document.getElementById('classementChart').getContext('2d');
+        new Chart(ctx, {
             type: 'line',
             data: {
                 labels: labels,
